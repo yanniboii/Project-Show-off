f0f7f3e8bc4d174b7d98efa9e46b6c91232920a7
yanniboii <yannekzessin@gmail.com>
2024-05-11 17:22:14 +0200

    controlls done

--- Assets/Scripts/Monster/BasicMovement.cs
+++ Assets/Scripts/Monster/BasicMovement.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Animations;
+using UnityEngine.InputSystem;
 public class BasicMovement : MonoBehaviour
 {
     [SerializeField] MonsterData monsterData;
     [SerializeField] Vector3 dir;
     Rigidbody rb;
+    Vector2 moveInput;
+    float jumpInput;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,36 +18,45 @@ public class BasicMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Move();   
-    }
-    
-    void GetInput()
-    {
+        rb.velocity = new Vector3(moveInput.x * monsterData.speed, jumpInput * monsterData.jumpHeight, moveInput.y * monsterData.speed);
+        
     }
 
-    void Move()
+    public void OnMove(InputAction.CallbackContext context)
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            rb.velocity = Vector3.forward * monsterData.speed;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            rb.velocity = Vector3.back * monsterData.speed;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            rb.velocity = Vector3.left * monsterData.speed;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.velocity = Vector3.right * monsterData.speed;
-        }
-        if (Input.GetKey(KeyCode.Space))
-        {
-            rb.velocity = Vector3.up * monsterData.jumpHeight;
-        }
+        moveInput = context.ReadValue<Vector2>();
     }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        jumpInput = context.ReadValue<float>();
+    }
+
+
+
+    //void Move()
+    //{
+    //    if (Input.GetKey(KeyCode.W))
+    //    {
+    //        rb.velocity = Vector3.forward * monsterData.speed;
+    //    }
+    //    if (Input.GetKey(KeyCode.S))
+    //    {
+    //        rb.velocity = Vector3.back * monsterData.speed;
+    //    }
+    //    if (Input.GetKey(KeyCode.A))
+    //    {
+    //        rb.velocity = Vector3.left * monsterData.speed;
+    //    }
+    //    if (Input.GetKey(KeyCode.D))
+    //    {
+    //        rb.velocity = Vector3.right * monsterData.speed;
+    //    }
+    //    if (Input.GetKey(KeyCode.Space))
+    //    {
+    //        rb.velocity = Vector3.up * monsterData.jumpHeight;
+    //    }
+    //}
 }
