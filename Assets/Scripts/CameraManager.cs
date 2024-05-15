565eeff3f7fe7b056c872eb108164a74d44c1571
Theodore Grist <theodoreg23456@gmail.com>
2024-05-15 10:42:12 +0200

    Camera controls
    Added an assignment of a camera when a player spawns in and started scripted camera movements

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -20,7 +20,6 @@ public class CameraManager : MonoBehaviour
         camera2.gameObject.SetActive(false);
         camera3.gameObject.SetActive(false);
         camera4.gameObject.SetActive(false);
-        Debug.Log("Camera1");
         camera1.rect = new Rect(0,0,1,1);
     }
     public void TwoPlayerCamera()
@@ -28,7 +27,6 @@ public class CameraManager : MonoBehaviour
         camera2.gameObject.SetActive(true);
         camera3.gameObject.SetActive(false);
         camera4.gameObject.SetActive(false);
-        Debug.Log("Camera2");
         camera1.rect = new Rect(0.25f, 0, 0.5f, 0.5f);
         camera2.rect = new Rect(0.25f, 0.5f, 0.5f, 0.5f);
     }
@@ -37,7 +35,6 @@ public class CameraManager : MonoBehaviour
         camera2.gameObject.SetActive(true);
         camera3.gameObject.SetActive(true);
         camera4.gameObject.SetActive(false);
-        Debug.Log("Camera3");
         camera1.rect = new Rect(0, 0.5f, 0.5f, 0.5f);
         camera2.rect = new Rect(0.5f, 0.5f, 0.5f, 0.5f);
         camera3.rect = new Rect(0.25f, 0, 0.5f, 0.5f);
@@ -47,7 +44,6 @@ public class CameraManager : MonoBehaviour
         camera2.gameObject.SetActive(true);
         camera3.gameObject.SetActive(true);
         camera4.gameObject.SetActive(true);
-        Debug.Log("Camera4");
         camera1.rect = new Rect(0, 0.5f, 0.5f, 0.5f);
         camera2.rect = new Rect(0.5f, 0.5f, 0.5f, 0.5f);
         camera3.rect = new Rect(0, 0, 0.5f, 0.5f);
