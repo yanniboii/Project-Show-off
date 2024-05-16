e4b3f3ea388ebe417e52473954184564d4b4f577
yanniboii <yannekzessin@gmail.com>
2024-05-16 13:28:48 +0200

    found the controllers need to filter
    InputSystem.Supporteddevices maybe

--- Assets/Scripts/JoinPlayers.cs
+++ Assets/Scripts/JoinPlayers.cs
@@ -1,25 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.LowLevel;
 
 public class JoinPlayers : MonoBehaviour
 {
     public PlayerInputManager input;
-    [SerializeField] InputDevice inputDevice1;
-    [SerializeField] InputDevice inputDevice2;
-    [SerializeField] InputDevice inputDevice3;
-    [SerializeField] InputDevice inputDevice4;
+    [SerializeField] GameObject playerPrefab;
+    int joinIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        input.EnableJoining();
-        input.JoinPlayer(0,0,null, inputDevice1);
-        input.JoinPlayer(1, 0, null, inputDevice2);
-        input.JoinPlayer(2, 0, null, inputDevice3);
-        input.JoinPlayer(3, 0, null, inputDevice4);
-
+        //InputSystem.onDeviceChange += (device, change) =>
+        //{
+        //    if(change == InputDeviceChange.Added)
+        //    {
+        //        input.JoinPlayer(joinIndex, 0, null, device);
+        //        GameObject player = Instantiate(playerPrefab);
+        //        PlayerInput.Instantiate(playerPrefab, joinIndex,null,-1, device);
+        //        joinIndex++;
+        //    }
+        //};
+        List<InputDevice> devices = InputSystem.devices.ToList();
+        foreach (InputDevice device in devices)
+        {
+            Debug.Log(device.ToString());
+            if (device.description.empty) 
+            {
+                Debug.Log("empty");
+                return; 
+            }
+//            if(InputSettings.supportedDevices.Contains(device.layout))
+            input.JoinPlayer(joinIndex, 0, null, device);
+            GameObject player = Instantiate(playerPrefab);
+            PlayerInput.Instantiate(playerPrefab, joinIndex, null, -1, device);
+            joinIndex++;
+        }
     }
 
     // Update is called once per frame
