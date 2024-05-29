ef42b20f98db5bf62886e7adc4fd25213e4fe473
Theodore Grist <theodoreg23456@gmail.com>
2024-05-29 12:26:24 +0200

    Moved a level into the hub world and resized it
    Moved my previous level into the hub, resized it to make more sense with current movement, added another material that's going to burn

--- Assets/Burning.cs
+++ Assets/Burning.cs
@@ -7,16 +7,15 @@ public class Burning : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.GetComponent<Renderer>() != null && collision.gameObject.GetComponent<Renderer>().material.name == "Woody (Instance)")
+        if (collision.gameObject.GetComponent<Renderer>() != null)
         {
-            Destroy(collision.gameObject);
-        }
-        else if (collision.gameObject.GetComponentInChildren<Renderer>(true).material.name == "Woody (Instance)")
-        {
-            Destroy(collision.gameObject);
-            if (this.name == "fireball(Clone)")
+            if (collision.gameObject.GetComponent<Renderer>().material.name == "Woody (Instance)" || collision.gameObject.GetComponent<Renderer>().material.name == "Grassy (Instance)")
             {
-                Destroy(this.gameObject);
+                Destroy(collision.gameObject);
+                if (this.name == "fireball(Clone)")
+                {
+                    Destroy(this.gameObject);
+                }
             }
         }
     }
