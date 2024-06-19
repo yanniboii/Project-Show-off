0616840b4514f4e6ecd7086f0130e17d193e5821
minimanx00 <msengers98@gmail.com>
2024-06-19 13:40:11 +0200

    talk sounds

--- Assets/Scripts/DesignerScripts/collectKey.cs
+++ Assets/Scripts/DesignerScripts/collectKey.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class collectKey : MonoBehaviour
 {
+    BasicMovement movementscript;
 
   [SerializeField]  AudioSource audioSource;
+
+  void Start (){
+    movementscript = GetComponent<BasicMovement>();
+  }
   void OnTriggerEnter(Collider other)
     {
     
         if (other.CompareTag("keycollectable"))
         {
             audioSource.Play();
+            movementscript.Talk();
             Destroy(other.gameObject);
 
 
